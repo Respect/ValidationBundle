9df4c583265dd458d5a4dd30992dc0489d35a39e
drgomesp <daniel@obiz.com.br>
2013-01-30 12:40:51 -0200

    Added tests to cover basic functionallity of the string validator

---
+++ Tests/Constraints/AssertValidatorTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Respect\ValidationBundle\Tests\Constraints;
+
+use Respect\Validation\Validator;
+use Respect\ValidationBundle\Validator\Constraints\Assert;
+use Respect\ValidationBundle\Validator\Constraints\AssertValidator;
+
+class AssertValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $context;
+    protected $validator;
+    protected $assertValidator;
+    
+    protected function setUp()
+    {
+        $this->context = $this->getMock('Symfony\Component\Validator\ExecutionContext', array(), array(), '', false);
+        $this->validator = Validator::create();
+        $this->assertValidator = new AssertValidator($this->validator);
+        $this->assertValidator->initialize($this->context);
+    }
+    
+    protected function tearDown()
+    {
+        $this->context = null;
+        $this->respectValidator = null;
+        $this->assertValidator = null;
+    }
+    
+    public function providerInvalidString()
+    {
+        return array(
+            array(false),
+            array(true),
+            //array(new \stdClass()), what about this one?
+            array(12345),
+            array(array()),
+        );
+    }
+    
+    /**
+     * @dataProvider providerInvalidString
+     */
+    public function testStringValidator($invalidString)
+    {   
+        $constraint = new Assert(array(
+            'options' => array(
+                'string' => array('')
+            )
+        ));
+
+        $this->context->expects($this->once())
+                      ->method('addViolation')
+                      ->with("\"$invalidString\" must be a string", array('{{ value }}' => $invalidString));
+        
+        $this->assertValidator->validate($invalidString, $constraint);
+    }
+}
